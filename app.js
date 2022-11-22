0000250e8a1fccd6a21e40181642296528cab5ed
Lucas Martins Menezes <lucasma@br.ibm.com>
2022-11-22 12:20:56 -0300

    set headers

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const bodyParser = require('body-parser');
 const bookRoutes = require('./routes/book');
 
 const app = express();
+
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use('/books', bookRoutes)
 
