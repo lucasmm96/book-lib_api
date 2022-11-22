4d767f0d2079f607868cf28014e6fde6f36696eb
Lucas Martins Menezes <lucasma@br.ibm.com>
2022-11-22 11:55:49 -0300

    get method

--- controllers/book.js
+++ controllers/book.js
@@ -1,3 +1,13 @@
-exports.getBooks = (req, res, next) => {
 
+
+exports.getBooks = (req, res, next) => {
+  res.status(200).json({
+    books: [
+      {title: 'book01', author: 'autor01'},
+      {title: 'book02', author: 'autor02'},
+      {title: 'book03', author: 'autor03'},
+      {title: 'book04', author: 'autor04'},
+      {title: 'book05', author: 'autor05'}
+    ]
+  });
 };
--- routes/book.js
+++ routes/book.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/book');
 
-router.get('/books', bookController.getBooks);
+router.get('/list', bookController.getBooks);
 
 module.exports = router;
